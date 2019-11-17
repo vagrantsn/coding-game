d919d5d3403d782671e054028220338f6d24de9e
Vagner S <vagnervst17@gmail.com>
2019-11-16 23:52:27 -0300

    chore: add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,40 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 9000,
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          'postcss-loader'
+        ],
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
       }
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    })
+  ],
 }
